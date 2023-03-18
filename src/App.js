5cb42490451a1362b533f8db807696dd2f10f2ec
danielTM999 <danielmelo101203@gmail.com>
2023-03-18 20:40:19 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/App.js
    
     Changes not staged for commit:
    	modified:   src/Components/Navbar.jsx
    	modified:   src/Components/RouteProvider.jsx
    	modified:   src/styles/Navbarstyle.css
    	modified:   src/styles/loginstyle.css
    
     Untracked files:
    	src/pages/Addprod.jsx
    	src/pages/admin/
    	src/styles/addprod.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Routes, Route } from "react-router-dom";
-import { RouteProvider, RouteProviderLog } from "./Components/RouteProvider";
+import { RouteProvider, RouteProviderLog, RouteProviderVend } from "./Components/RouteProvider";
 import Home from "./pages/Home.jsx";
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import Produto from './pages/Produto';
+import LogAdmin from './pages/admin/LogAdmin';
+import Addprod from './pages/Addprod';
 
 function App() {
   return (
@@ -19,13 +21,35 @@ function App() {
         />
         <Route
           path='/cadastro'
-          element={<RouteProviderLog><Cadastro /></RouteProviderLog >}
+          element={
+          <RouteProviderLog>
+            <Cadastro />
+          </RouteProviderLog >}
         />
 
         <Route
           path='/produto'
-          element={<RouteProvider><Produto /></RouteProvider>}
+          element={
+          <RouteProvider>
+            <Produto />
+          </RouteProvider>}
         />
+
+        <Route
+          path='/addprod'
+          element={
+          <RouteProvider>
+            <RouteProviderVend>
+              <Addprod />
+            </RouteProviderVend>
+          </RouteProvider>}
+        />
+
+        <Route
+          path='/auth/admin'
+          element={<LogAdmin />}
+        />
+
     </Routes>
   );
 }
